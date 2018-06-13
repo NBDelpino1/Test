1641628e264f35ef138a47f0d3a8200e1d35b038
Nicholas Delpino <NBDelpino4@gmail.com>
2018-06-13 11:28:48 -0400

    test

--- contact.php
+++ contact.php
@@ -1,3 +1,30 @@
+<?php
+// using SendGrid's PHP Library
+// https://github.com/sendgrid/sendgrid-php
+require 'vendor/autoload.php'; // If you're using Composer (recommended)
+// Comment out the above line if not using Composer
+// require("./sendgrid-php.php");
+// If not using Composer, uncomment the above line
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("nbdelpino1@yahoo.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("nbdelpino1@yahoo.com", "Example User");
+$email->addContent(
+    "text/plain", "and easy to do anywhere, even with PHP"
+);
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+?>
 
 <?php
     if (isset($_POST["submit"])) {
